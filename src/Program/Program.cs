878565d5a25a7f35300f764d6fe5ede0f2350c1e
AlfoRodriguez7 <alfonso_rodriguez_7@hotmail.com>
2023-09-24 16:59:49 -0300

    Compuertas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,23 +5,47 @@
 //--------------------------------------------------------------------------------
 
 using System;
-using Library;
+using Gates;
 
 namespace ConsoleApplication
 {
-    /// <summary>
-    /// Programa de consola de demostración.
-    /// </summary>
-    public static class Program
+    class Program
     {
-        /// <summary>
-        /// Punto de entrada al programa principal.
-        /// </summary>
-        public static void Main()
+        static void Main()
         {
-            var train = new Train();
-            train.StartEngines();
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el valor de la primera incógnita (true o false): ");
+            bool input1 = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de la segunda incógnita (true o false): ");
+            bool input2 = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de la tercera incógnita (true o false): ");
+            bool input3 = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Elija la operación lógica (AND, OR, NOT): ");
+            string operacion = Console.ReadLine().ToUpper();
+
+            switch (operacion)
+            {
+                case "AND":
+                    bool resultadoAnd = ANDGates.Evaluar(input1, input2);
+                    Console.WriteLine($"El resultado de {operacion} es: {resultadoAnd}");
+                    break;
+
+                case "OR":
+                    bool resultadoOr = ORGates.Evaluar(input1, input2);
+                    Console.WriteLine($"El resultado de {operacion} es: {resultadoOr}");
+                    break;
+
+                case "NOT":
+                    bool resultadoNot = NOTGates.Evaluar(input1, input2);
+                    Console.WriteLine($"El resultado de NOT({input1}) es: {resultadoNot}");
+                    break;
+
+                default:
+                    Console.WriteLine("Operación no válida.");
+                    break;
+            }
         }
     }
 }
