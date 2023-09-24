3a8fe735e7b1d31ec64af349685f21d650a52ca2
AgustinNegreira <agustin.negreira@correo.ucu.edu.uy>
2023-09-24 20:53:13 -0300

    Program.cs fix

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,38 +6,52 @@ namespace ConsoleApplication
 {
     class Program
     {
+        /* aqui en el program realizamos una implementacion en consola de las 3 compuertas logicas base
+        solicitadas :) */
         private static void Main()
         {
-            Console.WriteLine("Ingrese el valor de la primera incógnita (true o false): ");
-            bool input1 = bool.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el valor de la segunda incógnita (true o false): ");
-            bool input2 = bool.Parse(Console.ReadLine());
-
             Console.WriteLine("Elija la operación lógica (AND, OR, NOT): ");
             string operacion = Console.ReadLine().ToUpper();
 
             switch (operacion)
             {
                 case "AND":
-                    bool resultadoAnd = ANDGates.Evaluar(input1, input2);
+                    Console.WriteLine("Ingrese el valor de la primera entrada (true o false): ");
+                    bool input1and = bool.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el valor de la segunda entrada (true o false): ");
+                    bool input2and = bool.Parse(Console.ReadLine());
+
+                    ILogicGate andgate = new AndGate(input1and, input2and);
+                    bool resultadoAnd = andgate.Evaluate();
                     Console.WriteLine($"El resultado de {operacion} es: {resultadoAnd}");
                     break;
 
                 case "OR":
-                    bool resultadoOr = ORGates.Evaluar(input1, input2);
+                    Console.WriteLine("Ingrese el valor de la primera entrada (true o false): ");
+                    bool input1or = bool.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el valor de la segunda entrada (true o false): ");
+                    bool input2or = bool.Parse(Console.ReadLine());
+
+                    ILogicGate orgate = new OrGate(input1or, input2or);
+                    bool resultadoOr = orgate.Evaluate();
                     Console.WriteLine($"El resultado de {operacion} es: {resultadoOr}");
                     break;
 
                 case "NOT":
-                    bool resultadoNot = NOTGates.Evaluar(input1);
-                    Console.WriteLine($"El resultado de NOT({input1}) es: {resultadoNot}");
+                    Console.WriteLine("Ingrese el valor de la primera entrada (true o false): ");
+                    bool input = bool.Parse(Console.ReadLine());
+                    
+                    ILogicGate notgate = new NotGate(input);
+                    bool resultadoNot = notgate.Evaluate();
+                    Console.WriteLine($"El resultado de NOT({input}) es: {resultadoNot}");
                     break;
 
                 default:
                     Console.WriteLine("Operación no válida.");
                     break;
             }
+
+            
         }
     }
 }
